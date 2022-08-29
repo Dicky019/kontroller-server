7800e85fd6b205d3eccb2d6a3ad4f93808cf1b03
Dicky Darmawan <dicky93darmawan@gmail.com>
2022-08-29 21:47:54 +0800

    fix 1 login

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./skripsi-6f26d-firebase-adminsdk-v9vov-2478224e31.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://skripsi-6f26d-default-rtdb.asia-southeast1.firebasedatabase.app"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://skripsi-6f26d-default-rtdb.asia-southeast1.firebasedatabase.app",
 });
 
-const firestore = admin.firestore()
+const firestore = admin.firestore();
 
 const db = admin.database();
 
@@ -15,62 +16,59 @@ const db = admin.database();
 
 const refLampu = db.ref("/Humidity_lampu_isOn");
 const refFan = db.ref("/Temperatur_fan_isOn");
+const refLog = db.ref("/login");
 
-const topic = 'myTopic';
-const imageFan = 'https://raw.githubusercontent.com/Dicky019/kontroller-server/master/assets/fan.png'
-const imageLamp = 'https://raw.githubusercontent.com/Dicky019/kontroller-server/master/assets/lamp.png'
+const topic = "myTopic";
+const imageFan =
+  "https://raw.githubusercontent.com/Dicky019/kontroller-server/master/assets/fan.png";
+const imageLamp =
+  "https://raw.githubusercontent.com/Dicky019/kontroller-server/master/assets/lamp.png";
 
 const message = {
+  notification: {
+    title: "Info",
+    body: "",
+    // sound : "default"
+  },
+  android: {
+    priority: "high",
     notification: {
-        title: 'Info',
-        body: '',
-        // sound : "default"
+      imageUrl: "",
     },
-    android: {
-        priority: "high",
-        notification: {
-            imageUrl: '',
-        }
-    },
-    topic: topic
+  },
+  topic: topic,
 };
 
-firestore.collection("mac").onSnapshot(docSnapshot => {
-    docSnapshot.docs.forEach((element)=>{
-        const isLogin = element.data().isLogin;
-        const nama = element.data().nama;
-        // console.log(isLogin);
-        if (isLogin && nama != "admin") {
-            message.topic = nama
-            console.log(nama);
-            refLampu.on("value", (snapsot) => {
-                const data = snapsot.val();
-                if (data) {
-                    message.notification.body = "Lampu Anda Menyala"
-                    message.android.notification.imageUrl = imageLamp
-                    sendMessaging(message);
-                }
-            })
-            refFan.on("value", (snapsot) => {
-                const data = snapsot.val();
-                if (data) {
-                    message.notification.body = "Kipas Anda Menyala"
-                    message.android.notification.imageUrl = imageFan
-                    sendMessaging(message);
-                }
-            })
-        }
-    })
-  }, err => {
-    console.log(`Encountered error: ${err}`);
-  });
-
+refLog.on("value", (login) => {
+  if (login.val() != "") {
+    message.topic = login.val();
+    refLampu.on("value", (snapsot) => {
+      const data = snapsot.val();
+      if (data) {
+        message.notification.body = "Lampu Anda Menyala";
+        message.android.notification.imageUrl = imageLamp;
+        sendMessaging(message);
+      }
+    });
+    refFan.on("value", (snapsot) => {
+      const data = snapsot.val();
+      if (data) {
+        message.notification.body = "Kipas Anda Menyala";
+        message.android.notification.imageUrl = imageFan;
+        sendMessaging(message);
+      }
+    });
+  }
+});
 
 function sendMessaging(message) {
-    admin.messaging().send(message).then((response) => {
-        console.log('Successfully sent message:', response);
+  admin
+    .messaging()
+    .send(message)
+    .then((response) => {
+      console.log("Successfully sent message:", response);
     })
-        .catch((error) => {
-            console.log('Error sending message:', error);
-        });
+    .catch((error) => {
+      console.log("Error sending message:", error);
+    });
 }
